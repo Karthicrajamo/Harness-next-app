4253e33d530925bf1204b7238c0f8feec805b10d
Karthicrajamo <haikarthicraja4@gmail.com>
2025-12-16 13:17:27 +0530

    refactor: Update APIROUTES structure for better organization and clarity

--- src/lib/serverApi.ts
+++ src/lib/serverApi.ts
@@ -1,13 +1,12 @@
-import axios from "axios";
-import { cookies } from "next/headers";
+// import axios from "axios";
+// import { cookies } from "next/headers";
 
-export const serverApi = () => {
-  // const token = cookies().get("token")?.value; 
+// export const serverApi = () => {
+// const token = cookies().get("token")?.value;
 
-  // const instance = axios.create({
-  //   baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000/api",
-  //   headers: token ? { Authorization: `${token}` } : {},
-  // });
+//   return axios.create({
+//     baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000/api",
+//     headers: token ? { Authorization: token } : {},
+//   });
+// };
 
-  // return instance;
-};
