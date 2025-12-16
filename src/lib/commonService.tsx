a7420c4133569b8a1c5ff4c5f13ac1012664d860
Karthicrajamo <haikarthicraja4@gmail.com>
2025-12-16 13:06:19 +0530

    refactor: Update APIROUTES structure for better organization and clarity

--- src/lib/commonService.tsx
+++ src/lib/commonService.tsx
@@ -1,30 +1,30 @@
-// import api from "./axiosClient";
+import api from "./axiosClient";
 
-// export const getRequest = async (url, params) => {
-//   const res = await api.get(url, { params });
-//   return res;
-// };
+export const getRequest = async (url: string, params: any) => {
+  const res = await api.get(url, { params });
+  return res;
+};
 
-// export const postRequest = async (url, payload, params) => {
-//   console.log("first");
-//   const res = await api.post(url, payload, {
-//     params,
-//   });
-//   console.log(res, "=======>");
-//   return res;
-// };
+export const postRequest = async (url: string, payload: any, params: any) => {
+  console.log("first");
+  const res = await api.post(url, payload, {
+    params,
+  });
+  console.log(res, "=======>");
+  return res;
+};
 
-// export const putRequest = async (url, payload, params) => {
-//   const res = await api.put(url, payload, { params });
-//   return res;
-// };
+export const putRequest = async (url: string, payload: any, params: any) => {
+  const res = await api.put(url, payload, { params });
+  return res;
+};
 
-// export const deleteRequest = async (url, params) => {
-//   const res = await api.delete(url, { params });
-//   return res;
-// };
+export const deleteRequest = async (url: string, params: any) => {
+  const res = await api.delete(url, { params });
+  return res;
+};
 
-// export const patchRequest = async (url, payload, params) => {
-//   const res = await api.patch(url, payload, { params });
-//   return res;
-// };
+export const patchRequest = async (url: string, payload: any, params: any) => {
+  const res = await api.patch(url, payload, { params });
+  return res;
+};
